c613cd07a17431706ea45e658bc25523b38995c5
Your Name <you@example.com>
2019-06-12 19:36:30 -0300

    aula de dia 12

---
+++ src/br/ufpb/dcx/aps/aula2/App.java
@@ -0,0 +1,25 @@
+package br.ufpb.dcx.aps.aula2;
+
+public class App {
+
+	private static GenericService<Marca> marcaService = new GenericService<Marca>();
+	private static ModeloService modeloService = new ModeloService();
+
+	public static void main(String[] args) {
+		Marca vw = new Marca("Volkswagen");
+		int vwId = marcaService.cadastrar(vw);
+		Marca vwRetornada = marcaService.get(vwId);
+		
+		assert "Marca [nome=Volkswagen]".equals(vwRetornada.toString());
+
+		Modelo gol = new Modelo("Gol", vw);
+		int golId = modeloService.cadastrar(gol);
+		Modelo golRetornado = modeloService.get(golId);
+		
+		assert "Modelo [nome=Gol, marca=Marca [nome=Volkswagen]]"
+			.equals(golRetornado.toString());
+		
+		assert 1 == vw.getQuantidadeModelos();
+	}
+
+}
